5121e8585a53e96001cd563369738f234884ab92
Al Zaplin <zaplin.dev@gmail.com>
2025-12-02 13:08:34 +0300

    Feat: (Charts on JS) Create two charts, which fetching data from Yahoo Finance API... ... for creating one year price charts for S&P 500 and Bitcoin prices

--- js/script.js
+++ js/script.js
@@ -16,42 +16,96 @@ const loadSP = async () => {
 
     // Даты дней торгов акций S&P500
     const timestamp = result.timestamp;
-    console.log('timestamp', timestamp);
 
     // Цены закрытия торгов акций S&P500
     const prices = result.indicators.quote[0].close;
-    console.log('prices', prices);
 
-		const labels
+    // Чтобы перевести данные timestamp, которые у нас в получается с API в секундах, в даты нам нужно сперва перевести это в миллисекунды и для этого умножить на 1000. Пробежимся циклом по всему массиву и умножим каждое значение на 1000. А затем при помощи "toLocaleDateString" перевести всё это в локальное время.
+    const labels = timestamp.map((t) =>
+      new Date(t * 1000).toLocaleDateString('ru')
+    );
+
+    renderSPChart(labels, prices);
   } catch (error) {
     console.error(error.message);
   }
 };
 
-loadSP();
-
 // https://www.chartjs.org/
-/* const ctx = document.getElementById('myChart');
+const renderSPChart = (labels, prices) => {
+  const ctx = document.getElementById('chart-sp');
 
-new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels,
+      datasets: [
+        {
+          label: 'S&P 500',
+          data: prices,
+          borderWidth: 2,
+        },
+      ],
     },
-    tension: 0.3,
-    pointRadius: 0,
-  },
-}); */
+    options: {
+      tension: 0.3,
+      pointRadius: 0,
+    },
+  });
+};
+
+const loadBitcoin = async () => {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const url =
+    proxy +
+    'https://query1.finance.yahoo.com/v8/finance/chart/BTC-USD?interval=1d&range=12mo';
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    const result = data.chart.result[0];
+
+    const timestamp = result.timestamp;
+
+    const prices = result.indicators.quote[0].close;
+
+    const labels = timestamp.map((t) =>
+      new Date(t * 1000).toLocaleDateString('ru')
+    );
+
+    renderBitcoinChart(labels, prices);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const renderBitcoinChart = (labels, prices) => {
+  const ctx = document.getElementById('chart-bitcoin');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels,
+      datasets: [
+        {
+          label: 'Bitcoin',
+          data: prices,
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      tension: 0.3,
+      pointRadius: 0,
+      fill: true,
+      backgroundColor: 'rgb(51, 102, 255, .3)',
+    },
+  });
+};
+
+loadSP();
+loadBitcoin();
